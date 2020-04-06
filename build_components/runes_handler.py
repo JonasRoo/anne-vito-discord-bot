de410d69b34c8dcdf3715a5d33c6e3c599ec7733
unknown <jonas-roosen@t-online.de>
2020-04-06 14:58:16 +0200

    Fixed runes validation

--- build_components/runes_handler.py
+++ build_components/runes_handler.py
@@ -25,12 +25,12 @@ VALID_RUNE_LEVELS = ['level_one', 'level_two', 'level_three']
 
 def _validate_all_data() -> None:
     try:
-        _ = ALL_RUNES.empty
+        _ = len(ALL_RUNES)
         del _
     except:
         raise SystemError(f'{RUNES_RESOURCE_FILE_NAME} not properly loaded!')
     try:
-        _ = ALL_STATMODS.empty
+        _ = len(ALL_STATMODS)
         del _
     except:
         raise SystemError(f'{STATMOD_RESOURCE_FILE_NAME} not properly loaded!')
